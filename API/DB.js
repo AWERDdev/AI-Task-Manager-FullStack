f9ca6bb970c61ae06a96aad7686da519fcaf75cc
AWERDdev <kokorami565@gmail.com>
2025-04-15 13:54:03 +0200

    good changes

--- API/DB.js
+++ API/DB.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/TaskMaster');
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
