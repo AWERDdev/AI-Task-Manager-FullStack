27c67422718d83b63a1d4f8ff1bb7e95e2e28751
AWERDdev <kokorami565@gmail.com>
2025-04-16 03:15:16 +0200

    good work

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    {
-      apply: 'post',
-      enforce: 'post',
-      transformIndexHtml() {
-        return [
-          {
-            tag: 'link',
-            attrs: { rel: 'stylesheet', href: '/src/index.css' },
-            injectTo: 'head',
-          },
-        ]
-      },
-    },
+
   ],
 })
