bf69555f3087b0cbb77e6b06342444e9ec77bb81
AWERDdev <kokorami565@gmail.com>
2025-04-17 21:02:25 +0200

    New IDEA

--- client/src/Config.js
+++ client/src/Config.js
@@ -4,11 +4,11 @@ const isProduction = import.meta.env?.MODE === 'production';
 const config = {
   // API URLs
   nodeApiUrl: isProduction
-    ? 'https://your-public-node-server.com' // Replace with your public server URL
+    ? 'https://task-manager-backend-six-beige.vercel.app' // Replace with your public server URL
     : import.meta.env?.VITE_NODE_API_URL || 'http://localhost:3500',
 
   pythonApiUrl: isProduction
-    ? 'https://your-public-python-server.com' // Replace with your public server URL
+    ? 'https://task-manager-backend-data.com' // Replace with your public server URL
     : import.meta.env?.VITE_PYTHON_API_URL || 'http://127.0.0.1:8000',
 
   // Other configuration parameters
