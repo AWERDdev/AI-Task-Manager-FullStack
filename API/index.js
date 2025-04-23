367c058210e34a2341a44f643b8570e770037cb7
AWERDdev <kokorami565@gmail.com>
2025-04-23 23:11:08 +0200

    changes

--- API/index.js
+++ API/index.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: [
-    'https://task-manager-full-stack.vercel.app', 
+    'https://task-manager-full-stack-orcin.vercel.app/', 
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
