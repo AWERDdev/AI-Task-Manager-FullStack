6b3fa90591a0e1859f94201b05afc41eb4e0b572
AWERDdev <kokorami565@gmail.com>
2025-04-26 13:17:20 +0300

    Updated

--- API/DB.js
+++ API/DB.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 //mongodb://127.0.0.1:27017/TaskMaster
 const connectDB = async () => {
+  // 'mongodb://127.0.0.1:27017/TaskMasterPublic ' local route
   try {
-    await mongoose.connect(process.env.MONGODB_URI|| 'mongodb://127.0.0.1:27017/TaskMasterPublic ');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('MongoDB connection error:', error);
