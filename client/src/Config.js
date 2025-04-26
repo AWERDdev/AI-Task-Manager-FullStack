57d5f6a0c0bff841b5c1fa3034c1b01c7de7092e
AWERDdev <kokorami565@gmail.com>
2025-04-26 13:08:09 +0300

    good work

--- client/src/Config.js
+++ client/src/Config.js
@@ -4,13 +4,13 @@ const isProduction = import.meta.env?.MODE === 'production';
 const config = {
   // API URLs
   nodeApiUrl: isProduction
-    ? 'https://aff3-41-46-57-203.ngrok-free.app' // debuging
-    // ? '' // Replace with your public server URL
+    // ? 'https://aff3-41-46-57-203.ngrok-free.app' // debuging
+     ? 'https://quick-marion-awerddev-c9f60f67.koyeb.app/' // Replace with your public server URL
     : import.meta.env?.VITE_NODE_API_URL || 'http://localhost:3500',
 
   pythonApiUrl: isProduction
-  ? '' // debuging
-    // ? '' // Replace with your public server URL
+    // ? '' // debuging
+    ? '' // Replace with your public server URL
     : import.meta.env?.VITE_PYTHON_API_URL || 'http://127.0.0.1:8000',
 
   // Other configuration parameters
